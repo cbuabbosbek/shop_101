2c33c47bf35b67ece1b0b098a28adb6fb898215b
Abbosbek <cbuabbosbek@gmail.com>
2026-03-03 18:08:24 +0500

    done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,45 @@
+import { useEffect, useState } from "react";
+
 function Home() {
-    return (
-        <>
-            <h1>Home</h1>
-        </>
-    )
+  const [users, setUsers] = useState([]);
+  const [joke, setJoke] = useState(null);
+
+  const fetchUsers = () => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((json) => setUsers(json));
+    console.log("users fetched..");
+  };
+
+  useEffect(fetchUsers, []);
+  console.log(users);
+
+  const getRandomJoke = () => {
+    console.log("CLICKED");
+    fetch("https://official-joke-api.appspot.com/random_joke")
+      .then((res) => res.json())
+      .then((json) => setJoke(json));
+  };
+
+  return (
+    <>
+      <h1>Home</h1>
+      {users.map((user) => (
+        <div className="card">
+          <h1> {user.name} </h1>
+          <p> {user.email} </p>
+          <p> {user.phone} </p>
+        </div>
+      ))}
+
+      <hr />
+      <h1> {joke && joke.id} </h1>
+      <h1> {joke && joke.setup} </h1>
+      <h1> {joke && joke.punchline} </h1>
+      <button onClick={getRandomJoke}>Random JOKE</button>
+      <hr />
+    </>
+  );
 }
 
-export default Home
+export default Home;
--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,82 +1,96 @@
-
 const data = [
-    {
-        brand: 'Samsung',
-        model: 'S24',
-        price: 550,
-        image: "https://www.imagineonline.store/cdn/shop/files/iPhone_15_Pink_PDP_Image_Position-1__en-IN.jpg?v=1759733974"
-    },
-    {
-        brand: 'Samsung',
-        model: 'S26 Ultra',
-        price: 1890,
-        image: "https://www.imagineonline.store/cdn/shop/files/iPhone_15_Pink_PDP_Image_Position-1__en-IN.jpg?v=1759733974"
-    },
-    {
-        brand: 'iPhone',
-        model: '13 Pro',
-        price: 700,
-        image: "https://www.imagineonline.store/cdn/shop/files/iPhone_15_Pink_PDP_Image_Position-1__en-IN.jpg?v=1759733974"
-    },
-    {
-        brand: 'iPhone',
-        model: 'SE',
-        price: 380,
-        image: "https://www.imagineonline.store/cdn/shop/files/iPhone_15_Pink_PDP_Image_Position-1__en-IN.jpg?v=1759733974"
-    },
-    {
-        brand: 'iPhone',
-        model: 'SE',
-        price: 380,
-        image: "https://www.imagineonline.store/cdn/shop/files/iPhone_15_Pink_PDP_Image_Position-1__en-IN.jpg?v=1759733974"
-    }
-]
+  {
+    brand: "Samsung",
+    model: "S24",
+    price: 550,
+    image:
+      "https://www.imagineonline.store/cdn/shop/files/iPhone_15_Pink_PDP_Image_Position-1__en-IN.jpg?v=1759733974",
+  },
+  {
+    brand: "Samsung",
+    model: "S26 Ultra",
+    price: 1890,
+    image:
+      "https://www.imagineonline.store/cdn/shop/files/iPhone_15_Pink_PDP_Image_Position-1__en-IN.jpg?v=1759733974",
+  },
+  {
+    brand: "iPhone",
+    model: "13 Pro",
+    price: 700,
+    image:
+      "https://www.imagineonline.store/cdn/shop/files/iPhone_15_Pink_PDP_Image_Position-1__en-IN.jpg?v=1759733974",
+  },
+  {
+    brand: "iPhone",
+    model: "SE",
+    price: 380,
+    image:
+      "https://www.imagineonline.store/cdn/shop/files/iPhone_15_Pink_PDP_Image_Position-1__en-IN.jpg?v=1759733974",
+  },
+  {
+    brand: "iPhone",
+    model: "SE",
+    price: 380,
+    image:
+      "https://www.imagineonline.store/cdn/shop/files/iPhone_15_Pink_PDP_Image_Position-1__en-IN.jpg?v=1759733974",
+  },
+];
 
-function Shop () {
-    return (
-        <>
-            <div className="container">
-                <h1 className="text-danger">Shop</h1>
+function Shop() {
+  return (
+    <>
+      <div className="container">
+        <h1 className="text-danger">Shop</h1>
 
-                <div className="card-container d-flex gap-3">
-                    {
-                        data.map( ({ brand, model, price, image }) => {
-                            return (
-                                <>
-                                    <div class="card" style={{ width: "18rem" }}>
-                                    <img src={image} class="card-img-top" alt="rasm" />
-                                    <div class="card-body">
-                                        <h5 class="card-title"> {brand} </h5>
-                                        <p class="card-text"> {model} </p>
-                                        <p class="card-text"> ${price} </p>
-                                        <a href="#" class="btn btn-primary">Buy</a>
-                                    </div>
-                                    </div>
-                                </>
-                            )
-                        } )
-                    }
-
+        <div className="card-container d-flex gap-3">
+          {data.map(({ brand, model, price, image }) => {
+            return (
+              <>
+                <div class="card" style={{ width: "18rem" }}>
+                  <img src={image} class="card-img-top" alt="rasm" />
+                  <div class="card-body">
+                    <h5 class="card-title"> {brand} </h5>
+                    <p class="card-text"> {model} </p>
+                    <p class="card-text"> ${price} </p>
+                    <a href="#" class="btn btn-primary">
+                      Buy
+                    </a>
+                  </div>
                 </div>
+              </>
+            );
+          })}
+        </div>
+      </div>
 
-            </div>
-
-
-            <ul>
-
-                {
-                    data.map( ({brand, model, price}) => {
-                       
-                        return (
-                            <>
-                                <li> {brand} {model}: {price}$ </li>
-                            </>
-                        )
-                    } )
-                }
-            </ul>
-        </>
-    )
+      <ul>
+        {data.map(({ brand, model, price }) => {
+          return (
+            <>
+              <li>
+                {" "}
+                {brand} {model}: {price}${" "}
+              </li>
+            </>
+          );
+        })}
+      </ul>
+    </>
+  );
 }
 
-export default Shop
+export default Shop;
+
+// return (
+//     <>
+//         <div class="card" style={{ width: "18rem" }}>
+//         <img src={image} class="card-img-top" alt="rasm" />
+//         <div class="card-body">
+//             <h5 class="card-title"> {brand} </h5>
+//             <p class="card-text"> {model} </p>
+//             <p class="card-text"> ${price} </p>
+//             <a href="#" class="btn btn-primary">Buy</a>
+//         </div>
+//         </div>
+//     </>
+// )
